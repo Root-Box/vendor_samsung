95776a6a19fead8263e09b1d77355c747744c751
Bajee11 <bajee11@gmail.com>
2012-12-05 00:37:18 -0500

    d2att: Update Adreno blobs

--- d2att/d2att-vendor-blobs.mk
+++ d2att/d2att-vendor-blobs.mk
@@ -48,12 +48,15 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2att/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
         vendor/samsung/d2att/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
         vendor/samsung/d2att/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+        vendor/samsung/d2att/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
         vendor/samsung/d2att/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
         vendor/samsung/d2att/proprietary/lib/libgsl.so:system/lib/libgsl.so \
         vendor/samsung/d2att/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
         vendor/samsung/d2att/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
         vendor/samsung/d2att/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
         vendor/samsung/d2att/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+        vendor/samsung/d2att/proprietary/lib/libsc-a3xx.so:system/lib/libsc-a3xx.so \
+        vendor/samsung/d2att/proprietary/lib/libllvm-a3xx.so:system/lib/libllvm-a3xx.so \
         vendor/samsung/d2att/proprietary/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
         vendor/samsung/d2att/proprietary/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
         vendor/samsung/d2att/proprietary/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
@@ -63,6 +66,7 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2att/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
         vendor/samsung/d2att/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
         vendor/samsung/d2att/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+        vendor/samsung/d2att/proprietary/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
         vendor/samsung/d2att/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
         vendor/samsung/d2att/proprietary/bin/netmgrd:system/bin/netmgrd \
         vendor/samsung/d2att/proprietary/bin/sec-ril:system/bin/sec-ril \
@@ -155,5 +159,4 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2att/proprietary/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
         vendor/samsung/d2att/proprietary/etc/wifi/nvram_net.txt_murata:system/etc/wifi/nvram_net.txt_murata \
         vendor/samsung/d2att/proprietary/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-        vendor/samsung/d2att/proprietary/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
         vendor/samsung/d2att/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so
